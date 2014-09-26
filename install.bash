6d650f77a82f7ee524ec0883185c0f18ad6e526b
Nathaniel Hellabyte <nate@hellabit.es>
2014-09-26 16:59:06 -0700

    :)

--- install.bash
+++ install.bash
@@ -3,7 +3,30 @@
 # Installs configuration files to User's home directory
 # TODO - Backup files that are being replaced
 
-TARBALL="./configuration_files.tar.gz"
-TARGET="$HOME"
+force_opt=${1:-''}
+srcdir="./hellabyte_dotfiles"
+tardir="$HOME"
 
-tar -C $TARGET -xzvf $TARBALL 
+echo "Default is to not force the install."
+echo "Rerun the bash install with 1 for the \$1 argument to force the
+      install"
+
+if [[ $force_opt == '' ]]; then
+  cp -r $srcdir/* $tardir
+  echo "Unforced copy complete"
+else if [[ $force_opt == '1' ]] then
+  cp -rf $srcdir/* $tardir
+  echo "Forced copy complete"
+else
+  echo "The first argument that was passed was: $1"
+  echo "This is not equivalent to 1 or blank."
+  echo "If \$1 (the first argument passed to the script) is set to 1,"
+  echo "then the copy will be forced and the files from here will "
+  echo "replace defaults."
+  echo ""
+  echo "If a parameter was passed to this install script by mistake,"
+  echo "Please try again without the parameter. "
+  exit 3
+fi
+
+
