0e53948068d09945bb65c876074103ad80a03ba1
Nathaniel Hellabyte <nate@hellabit.es>
2016-04-28 20:25:05 -0700

    :)

--- hellabyte_dotfiles/.local/etc/profile.d/hellabyte-source2-program-configs.sh
+++ hellabyte_dotfiles/.local/etc/profile.d/hellabyte-source2-program-configs.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 ## git fancy log
 ## git tags
 ## git status -sb
-
 config_personal() {
   git config --global alias.lg "log --color --graph
     --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)
