6eea01e00c41c59c606681f781d696562207d45c
Jason Yalim <yalim@asu.edu>
2022-01-20 08:34:40 -0700

    macos 11 /opt/homebrew

--- install.bash
+++ install.bash
@@ -16,28 +16,28 @@ now=$(date +"%Y.%m.%d-%H.%M")
 spcdir="${bakdir}/${now}"
 procs=4 # Assume 4 threads for xargs
 
-if ! [[ -d $spcdir ]]; then
+if ! [[ -d "$spcdir" ]]; then
   echo "------"
   echo "Making Backup directory -- $spcdir" 
   echo "This will be automatically deleted if no backups are made"
-  mkdir -pv $spcdir
+  mkdir -pv "$spcdir"
   echo "------"
 fi
 
-for d in $(find $upkdir -type d -print); do
+for d in $(find "$upkdir" -type d -print); do
   relative_dname="${d#*/}"
   target_dname="${tardir}/${relative_dname}"
-  if ! [[ -d $target_dname ]]; then
-    mkdir -pv $target_dname
+  if ! [[ -d "$target_dname" ]]; then
+    mkdir -pv "$target_dname"
   fi
 done #| xargs -P $procs -I {} echo {}
 
-for f in $(find $upkdir -type f -print); do 
+for f in $(find "$upkdir" -type f -print); do 
   relative_fname="${f#*/}"
   target_fname="${tardir}/${relative_fname}"
-  if ! [[ -e $target_fname ]]; then
+  if ! [[ -e "$target_fname" ]]; then
     echo "$target_fname not found, freshly installing"
-    cp -v $f $target_fname
+    cp -v "$f" "$target_fname"
   else
     diff_res=$(diff -q $f $target_fname)
     if ! [[ -z $diff_res ]]; then
@@ -45,19 +45,19 @@ for f in $(find $upkdir -type f -print); do
       relative_dname="${relative_fname%/*}"
       bakspcdir="${spcdir}/${relative_dname}"
       bakspcfile="${spcdir}/${relative_fname}"
-      mkdir -pv $bakspcdir
-      mv -v $target_fname $bakspcfile
-      cp -v $f $target_fname
+      mkdir -pv "$bakspcdir"
+      mv -v "$target_fname" "$bakspcfile"
+      cp -v "$f" "$target_fname"
     fi
   fi
 done #| xargs -P $procs -I {} echo {}
 
 # Check to see if backup was actually done
-readarray backup_check < <(ls -A1 $spcdir)
+readarray backup_check < <(ls -A1 "$spcdir")
 nBack=${#backup_check[@]}
 echo ""
-if [[ $nBack -eq 0 ]]; then
-  rm -r $spcdir
+if (( "$nBack" == 0 )); then
+  rm -r "$spcdir"
   echo "No backups were made."
 else
   echo "${nBack} backups were put in the following directory: $spcdir"
